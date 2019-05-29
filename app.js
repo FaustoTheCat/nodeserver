9e69d31de3b5b799422202f110b79e44bd2bf0e3
Bartkowiak <karsten.bartkowiak@sealsystems.de>
2019-05-29 11:06:42 +0200

    next commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const server = require('http');
+
+const  express = require('express');
+const  app = express(),
+       port = 3000;
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port '  + port);
+});
